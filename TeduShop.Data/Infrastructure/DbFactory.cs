fcc717b3ed850b5943e9aac729096bd333616602
Ông Tấn Mạnh <gaumapxipo@gmail.com>
2016-07-10 13:15:46 +0700

    Add Db

---
+++ TeduShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace TeduShop.Data.Infrastructure
+{
+    //Khởi tạo đối tượng thay vì new trực tiếp
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private TeduShopDbContext dbContext;
+
+        public TeduShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new TeduShopDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
---
+++ TeduShop.Data/Infrastructure/IDbFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Data.Infrastructure
+{
+    //Giao tiếp để khởi tạo đối tượng entity
+    public interface IDbFactory : IDisposable
+    {
+        TeduShopDbContext Init();
+    }
+}
