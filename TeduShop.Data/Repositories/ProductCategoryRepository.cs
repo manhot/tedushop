49b461f03b56ba9ab386fdf4496f52b69c5daac6
Ông Tấn Mạnh <gaumapxipo@gmail.com>
2016-07-12 21:34:34 +0700

    change data projects

---
+++ TeduShop.Data/Repositories/ProductCategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    //Định nghĩa phương thức cần thêm
+    public interface IProductCategoryRepository
+    {
+        IEnumerable<ProductCategory> GetByAlias(string alias);
+    }
+
+    public class ProductCategoryRepository : RepositoryBase<ProductCategory>, IProductCategoryRepository
+    {
+        //DbFactory phải truyền vào constructor vì repositoryBase phải có DbFactory
+        public ProductCategoryRepository(IDbFactory dbFactory) 
+            : base(dbFactory)
+        {
+
+        }
+
+        public IEnumerable<ProductCategory> GetByAlias(string alias)
+        {
+            return this.DbContext.ProductCategories.Where(x => x.Alias == alias);
+        }
+    }
+}
---
+++ TeduShop.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IProductRepository
+    {
+
+    }
+
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    { 
+        public ProductRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+
+        }
+    }
+}
